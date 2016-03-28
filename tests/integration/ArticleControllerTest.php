0331856ccefe04c8c87c79e844356900be9b4936
Loïc Mathaud <loic@mathaud.fr>
2016-03-28 19:44:57 +0200

    Add tests for GET /article/{id}

--- tests/integration/ArticleControllerTest.php
+++ tests/integration/ArticleControllerTest.php
@@ -6,4 +6,21 @@ class ArticleControllerTest extends TestCase
 {
     use DatabaseMigrations;
 
+    /** @test */
+    public function it_can_fetch_an_article()
+    {
+        # 1. Mettre en place le contexte
+        $article = factory(App\Article::class)->create();
+
+        # 2. Effectuer l'appel sur l'API
+        $this->get('/article/' . $article->id);
+
+        # 3. Réaliser les assertions
+        $this->seeStatusCode(200);
+        $this->seeJson([
+            'id' => $article->id,
+            'title' => $article->title,
+            'description' => $article->description,
+        ]);
+    }
 }
